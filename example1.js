fb709cd24c1db86bd4ace1218173f0cc83a48a63
blasty3 <udayanto.atmojo@gmail.com>
2018-07-09 15:01:57 +0300

    Signed-off-by: blasty3 <udayanto.atmojo@gmail.com>

--- example1.js
+++ example1.js
@@ -50,6 +50,30 @@ requirejs(['./worldwind.min',
             layers[l].layer.enabled = layers[l].enabled;
             wwd.addLayer(layers[l].layer);
         }
+        
+
+        // The common gesture-handling function.
+        var handleClick = function (recognizer) {
+            // Obtain the event location.
+            var x = recognizer.clientX,
+                y = recognizer.clientY;
+
+            // Perform the pick. Must first convert from window coordinates to canvas coordinates, which are
+            // relative to the upper left corner of the canvas rather than the upper left corner of the page.
+            var pickList = wwd.pick(wwd.canvasCoordinates(x, y));
+
+            // If only one thing is picked and it is the terrain, tell the WorldWindow to go to the picked location.
+            if (pickList.objects.length === 1 && pickList.objects[0].isTerrain) {
+                var position = pickList.objects[0].position;
+                wwd.goTo(new WorldWind.Location(position.latitude, position.longitude));
+            }
+        };
+
+        // Listen for mouse clicks.
+        var clickRecognizer = new WorldWind.ClickRecognizer(wwd, handleClick);
+
+        // Listen for taps on mobile devices.
+        var tapRecognizer = new WorldWind.TapRecognizer(wwd, handleClick);
 
         // Create a layer manager for controlling layer visibility.
         var layerManager = new LayerManager(wwd);
