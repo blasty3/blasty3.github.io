0404f4cf3b0ab155a7b1b59337661fbe689f2424
Atmojo Udayanto <udayanto.atmojo@aalto.fi>
2018-07-18 11:42:30 +0300

    Signed-off-by: Atmojo Udayanto <udayanto.atmojo@aalto.fi>

--- FetchData.js
+++ FetchData.js
@@ -25,6 +25,8 @@ requirejs([],
 		//var checkBoxTS = document.getElementById("thingspeakcheckbox");
 		
 		 if (checkBoxPA.checked == true){
+			 
+			 window.alert("PA checked!");
 
 				  var url = "https://www.purpleair.com/json";
 				 
@@ -37,6 +39,7 @@ requirejs([],
 				    "sensorsActive2" : 10080
 				  };
 			 
+				  
 				  
 				  var encParams = toHtmlQuery_(apiParams);
 				  
@@ -58,13 +61,15 @@ requirejs([],
 	    req.send();
 	    req.addEventListener("readystatechange", processReq, false);
 	    
+	    
+	    
 	    req.onreadystatechange = processRequest;
 	    
 	    function processRequest(e) {
 	     
 	    	if (req.readyState == 4 && req.status == 200) {
 	            var resp = JSON.parse(req.responseText);
-	            alert(resp.mapVersion);
+	            window.alert(resp.mapVersion);
 	        }
 	    	
 	    }
